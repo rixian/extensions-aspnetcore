0b667924443010ebe76141d5252e3d509ce64c24
Samuel McAravey <mcaravey@rixian.net>
2020-01-01 07:55:21 -0800

    Updated for missed property.

--- src/Rixian.Extensions.AspNetCore.Sample.Api/WeatherForecast.cs
+++ src/Rixian.Extensions.AspNetCore.Sample.Api/WeatherForecast.cs
@@ -13,6 +13,6 @@ namespace Rixian.Extensions.AspNetCore.Sample.Api
 
         public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
